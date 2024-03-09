7d7e3de1688f4b8a8d4e3a3e91f206acd8f48441
nhathao090703@gmail.com <21020196@vnu.edu.vn>
2024-03-09 14:46:48 +0700

    add OpenSUSE setup

---
+++ playbooks/scripts/i_vivid.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -q "https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb"
+sudo dpkg -i vivid_0.8.0_amd64.deb
+rm vivid_0.8.0_amd64.deb
--- scripts/i_vivid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget "https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb"
-sudo dpkg -i vivid_0.8.0_amd64.deb
-rm vivid_0.8.0_amd64.deb
